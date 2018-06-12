5b2180b5fd4ca72526743ad918b9a4d4961af2b6
Brian Lai <brian.lai.1992@gmail.com>
2018-06-11 22:24:18 -0400

    THIS IS MINE - I DID THIS

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,17 +1,43 @@
-import jsdom from 'jsdom';
+// import chai from 'chai';
+// import chaiImmutable from 'chai-immutable';
+
+// const jsdom = require('jsdom');
+// const { JSDOM } = jsdom;
+
+// const { win } = new JSDOM('<!doctype html><html><body></body></html>');
+// const { doc } = (new JSDOM('<!doctype html><html><body></body></html>')).window;
+
+// global.document = doc;
+// global.window = win;
+
+// Object.keys(window).forEach((key) => {
+//   if (!(key in global)) {
+//     global[key] = window[key];
+//   }
+// });
+
+// chai.use(chaiImmutable);
+
+
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-const win = doc.defaultView;
+const dom = new JSDOM('<!doctype html><html><body></body></html>');
+const win = dom.window;
+const doc = win.document;
 
 global.document = doc;
 global.window = win;
 
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
-    global[key] = window[key];
+      global[key] = window[key];
   }
 });
+
 
 chai.use(chaiImmutable);
